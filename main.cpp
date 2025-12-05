e483a5701c67ae8ec2d80138c122adc51e5cfcfc
abrilordonezvelasco <abrilordonezvelasco@gmail.com>
2025-12-05 09:39:06 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include  <iostream
+#include  <iostream>
 using namespace std;
 
 int main() {
   int num;
-cout << "tyoe a num" << endl;
+cout << "type a num" << endl;
 cin > num;
 cout << num << endl;
   return 0;
